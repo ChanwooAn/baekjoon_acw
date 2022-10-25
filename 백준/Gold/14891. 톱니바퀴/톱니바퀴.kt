b15262db4525635a47e2f321c4573d87e45f92e3
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-10-26 05:13:43 +0900

    [Gold V] Title: 톱니바퀴, Time: 152 ms, Memory: 21456 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.kt
@@ -0,0 +1,111 @@
+import kotlin.math.pow
+
+
+//시계방향은 오른쪽shift 반시계는 왼쪽 shift
+const val RIGHT = 5
+const val LEFT = 1
+
+
+var wheel = Array(4) { 0 }
+var visit = Array(4) { false }
+var turn = arrayListOf<Pair<Int, Int>>()
+
+
+fun addTurn(w: Int, turnD: Int) {
+
+
+    var wheelNowRight = if (wheel[w] and (1 shl RIGHT) != 0) 1 else 0
+    var wheelNowLeft = if (wheel[w] and (1 shl LEFT) != 0) 1 else 0
+
+    if (w - 1 >= 0) {
+        val leftWheelsRight = if (wheel[w - 1] and (1 shl RIGHT) != 0) 1 else 0
+
+        if (wheelNowLeft != leftWheelsRight && !visit[w - 1]) {
+            visit[w - 1] = true
+            turn.add(Pair(w - 1, turnD * -1))
+            addTurn(w - 1, turnD * -1)
+        }
+    }
+
+    if (w + 1 < 4) {
+        val rightWheelsLeft = if (wheel[w + 1] and (1 shl LEFT) != 0) 1 else 0
+        if (wheelNowRight != rightWheelsLeft && !visit[w + 1]) {
+            visit[w + 1] = true
+            turn.add(Pair(w + 1, turnD * -1))
+            addTurn(w + 1, turnD * -1)
+        }
+    }
+
+
+}
+
+fun turnWheel() {
+
+    while (turn.isNotEmpty()) {
+        val (w, d) = turn.removeFirst()
+
+        if (d == -1) {
+            //반시계
+            val last = wheel[w] and (1 shl 7)
+
+            wheel[w] = (wheel[w] shl 1) and 255
+            if (last != 0) {
+                wheel[w]++
+            }
+
+        } else {
+            //시계
+            val first = wheel[w] and 1
+
+            wheel[w] = wheel[w] shr 1
+
+            if (first != 0) {
+                wheel[w] += 1 shl 7
+            }
+        }
+
+
+    }
+
+}
+
+
+@OptIn(ExperimentalStdlibApi::class)
+fun main() {
+    repeat(4) {
+        var input = readln()
+        var m = 128
+        var num = 0
+        for (i in input) {
+            num += (i.digitToInt() * m)
+            m /= 2
+        }
+        wheel[it] = num
+
+    }
+
+    val turnCnt = readln().toInt()
+    repeat(turnCnt) {
+        val (w, d) = readln().split(" ").map { it.toInt() }
+        turn.add(Pair(w-1, d))
+        visit[w-1] = true
+
+
+        addTurn(w-1, d)
+        turnWheel()
+        visit = Array(4) { false }
+    }
+
+
+    var answer = 0
+    for (i in 0 until 4) {
+        if ((wheel[i] and (1 shl 7)) != 0) {
+            answer += (2.0).pow(i).toInt()
+        }
+    }
+
+
+    println(answer)
+
+
+}
