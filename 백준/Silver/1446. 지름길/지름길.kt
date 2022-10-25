06ed5824001a9fe447c8215da367f6555438f229
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-10-26 04:27:13 +0900

    [Silver I] Title: 지름길, Time: 292 ms, Memory: 27152 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.kt
+++ 백준/Silver/1446. 지름길/지름길.kt
@@ -19,10 +19,12 @@ fun main(){
         if(e>D){
             continue
         }
+        //목표보다 높은 지점을 향하는 지름길은 받지 않기
 
         if(c>=(e-s)){
             continue
         }
+        //지름길이 cost가 더 클경우도 받지 않기
 
         roads.add(Triple(s,e,c))
     }
@@ -31,12 +33,10 @@ fun main(){
         val (s,e,c)=roads.poll()
         if(distance[e]-distance[s]>c){
             distance[e]=distance[s]+c
-        }
-
-        for(j in e+1..D){
-            if(distance[j-1]+1<distance[j]){
-                distance[j]=distance[j-1]+1
-
+            for(j in e+1..D){
+                if(distance[j-1]+1<distance[j]){
+                    distance[j]=distance[j-1]+1
+                }
             }
         }
     }
