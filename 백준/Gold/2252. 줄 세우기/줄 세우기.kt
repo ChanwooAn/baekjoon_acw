e5d0eeee654dc453e7bbb2a3e45a86d1ec7d2f72
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2023-02-09 13:02:13 +0900

    [Gold III] Title: 줄 세우기, Time: 744 ms, Memory: 70616 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.kt
@@ -0,0 +1,39 @@
+
+val q=ArrayList<Int>()
+lateinit var indegreeArr:Array<Int>
+lateinit var graph:Array<ArrayList<Int>>
+
+fun main(){
+    val (n,m)= readln().split(" ").map { it.toInt() }
+    val answer= arrayListOf<Int>()
+    indegreeArr=Array(n+1){0}
+    graph=Array(n+1){ arrayListOf() }
+
+    repeat(m){
+        val (before,after)= readln().split(" ").map { it.toInt() }
+        indegreeArr[after]++
+        graph[before].add(after)
+    }
+
+    for(i in 1..n){
+        if(indegreeArr[i]==0) q.add(i)
+    }
+
+
+    while(q.isNotEmpty()){
+        val now=q.removeLast()
+        answer.add(now)
+
+        for(i in graph[now]){
+            indegreeArr[i]--
+
+            if(indegreeArr[i]==0){
+                q.add(i)
+            }
+        }
+
+    }
+
+    println(answer.joinToString(" "))
+
+}
